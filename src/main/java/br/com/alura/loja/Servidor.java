cd9afc050f9c52cacf05bcd823bc898c65eab936
BRQ\alessandrolima <alessandrolima@brq.com>
2018-03-07 17:18:04 -0300

    Alterando teste

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -9,21 +9,21 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Servidor {
 
-    public static void main(String[] args) throws IOException {
-        HttpServer server = startServer();
-        System.out.println("Servidor rodando");
-        System.in.read();
-        stopServer(server);
-    }
+	public static void main(String[] args) throws IOException {
+		HttpServer server = startServer();
+		System.out.println("Servidor rodando");
+		System.in.read();
+		stopServer(server);
+	}
 
-	public static void stopServer(HttpServer server) {
+	static void stopServer(HttpServer server) {
 		server.stop();
 	}
 
-	public static HttpServer startServer() {
+	static HttpServer startServer() {
 		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-        URI uri = URI.create("http://localhost:8080/");
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		URI uri = URI.create("http://localhost:8080/");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 		return server;
 	}
 
