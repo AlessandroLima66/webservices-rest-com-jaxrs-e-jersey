c70cc4cee48ac945263936cf88383c1b62e3717c
BRQ\alessandrolima <alessandrolima@brq.com>
2018-03-08 16:32:31 -0300

    Aula 5 -

--- src/main/java/br/com/alura/loja/ProjetoTest.java
+++ src/main/java/br/com/alura/loja/ProjetoTest.java
@@ -2,7 +2,10 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -11,6 +14,8 @@ import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
 
@@ -26,15 +31,32 @@ private HttpServer server;
     public void mataServidor() {
 		Servidor.stopServer(server);
     }
+	
 	@Test
 	public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos").request().get(String.class);
-		
+		String conteudo = target.path("/projetos/1").request().get(String.class);
 		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 		
 		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+	
+	@Test
+	public void testaPostDeCarrinhos() {
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+    
+        Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 2));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 
 }
