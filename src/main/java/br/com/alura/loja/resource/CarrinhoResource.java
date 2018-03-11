720f5f827572a837b835864ea3900c90e7b1a78a
Alessandro <ale.limaalexandre@gmail.com>
2018-03-11 18:56:45 -0300

    Aula 09 - Removendo recursos e subrecursos

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -35,5 +36,15 @@ public class CarrinhoResource {
 		
 		URI uri = URI.create("/carrinhos/" + carrinho.getId());
 		return Response.created(uri).build();
+	}
+	
+	@Path("{idCarrinho}/produtos/{idProduto}")
+	@DELETE
+	public Response remove(@PathParam("idCarrinho") long idCarrinho, 
+			@PathParam("idProduto") long idProduto ) {
+		Carrinho carrinho = new CarrinhoDAO().busca(idCarrinho);
+		carrinho.remove(idProduto);
+		
+		return Response.ok().build();
 	}
 }
--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -14,6 +15,7 @@ import javax.ws.rs.core.Response;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 
 @Path("projetos")
@@ -35,5 +37,13 @@ public class ProjetoResource {
 		
 		URI uri = URI.create("/projetos/" + projeto.getId());
 		return Response.created(uri).build();
+	}
+	
+	@Path("idProduto")
+	@DELETE
+	public Response remover(@PathParam("id") long id) {
+		new ProjetoDAO().remove(id);
+		
+		return Response.ok().build();
 	}
 }
